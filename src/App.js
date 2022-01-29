62059f9e21be5c072a6dbe5add7786e15015d431
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-01-29 17:10:06 +0330

    start building a modal in React.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
-import "./App.css";
-import Card from "./card";
+import React from 'react';
 
-const App = (props) => {
-  // first state  
-  const [CardName, SetCardName] = useState({
-    Name: [{ name: "Bigman" }, { name: "majid" }, { name: "Japan" }],
-  });
-  // set stat with hooks 
-  const ForClick = () => {
-    SetCardName({
-      Name: [{ name: "John" }, { name: "Ali" }, { name: "Japman" }],
-    });
-  };
-
-  return (
-    <>
-      <div className="title">
-        <h1>image gallary</h1>
-        <hr />
-      </div>
-      <div className="container">
-        <Card name={CardName.Name[1].name} />
-        <Card name={CardName.Name[0].name} />
-        <Card name={CardName.Name[2].name} />
-        <Card name={CardName.Name[0].name} />
-      </div>
-      <button onClick={ForClick}>change name</button>
-    </>
-  );
-};
-// class App extends React.Component {
-//   state =
-//   ForClick = () => {
-
-//   };
-
-//   render() {
-//
-//   }
-// }
-
+const App = () => {
+    return (
+        // React Fragment 
+        <>
+            let goooooooo !
+        </>
+    )
+}
 export default App;
