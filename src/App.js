34d018ad867cc7e2e4bcbe9d2974cc66b7ae9b75
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-01-29 23:12:05 +0330

    commenting and end

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./App.css";
 import ModalExampleDimmer from "./Components/modal/Modal";
 
+// I tried to do the whole coding part of this modal in a
+// separate component so that this page is as empty as possible.
+// In this section, I just import the components and use them.
+// I use CSS in this project only to create flex.
+
 const App = () => {
-  return (
-    // React Fragment
-    <>
-      {/* modal component */}
-      <ModalExampleDimmer />
-    </>
-  );
+  return <ModalExampleDimmer />;
 };
 
 export default App;
